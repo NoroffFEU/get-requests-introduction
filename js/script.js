2b12327aeea0f2c84763244ace7e52252edef9c8
cnnrbrn <connorbobrien@gmail.com>
2020-08-20 23:13:37 +0200

    Add first files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const url = "https://cat-fact.herokuapp.com/facts";
+
+const resultsContainer = document.querySelector(".results");
+
+async function getCats() {
+    // fetch
+
+    const response = await fetch(url);
+
+    const results = await response.json();
+
+    const facts = results.all;
+
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i < facts.length; i++) {
+        console.log(facts[i].text);
+
+        if (i === 10) {
+            break;
+        }
+
+        resultsContainer.innerHTML += `<div class="result">${facts[i].text}</div>`;
+    }
+}
+
+getCats();
